45ca0f90857154f18572c9aa5ea5e50278ebd0c1
neanyameee <anna_klimova_06@mail.ru>
2025-10-17 00:55:08 +0500

    8

--- book_project/books/models.py
+++ book_project/books/models.py
@@ -4,4 +4,3 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     year = models.CharField(max_length=4)
-    isbn = models.CharField(max_length=13)
